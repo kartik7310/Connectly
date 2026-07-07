d5f21390fee4925e603d76e709090f5397ad0500
kartik lathiyan <kartiklathiyan455@gmail.com>
2026-07-07 15:35:08 +0530

    fix smtp issue

--- Backend/src/config/nodemailer.js
+++ Backend/src/config/nodemailer.js
@@ -21,13 +21,13 @@ import config from "./env.js";
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false, // Must remain false for 587, but we enforce TLS below
+  port: 443,
+  secure: true,
   auth: {
     user: config.nodemailer.brevoLogin,
     pass: config.nodemailer.brevoPassword,
   },
-  requireTLS: true, // Forces Nodemailer to use STARTTLS immediately
+  requireTLS: true,
   tls: {
     // Allows local development without strict certs, but enforces secure handshake on Render
     rejectUnauthorized: process.env.NODE_ENV === "production" ? true : false
